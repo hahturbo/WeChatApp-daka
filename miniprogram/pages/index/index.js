dbb7c3714e97c60baa17c037e9b9444058aa86af
zzh01028 <1409415615@qq.com>
2020-06-10 22:32:30 +0800

    1.0.0

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -804,8 +804,11 @@ Page({
 
   ChangePageGetBoard: function (e) {
     this.GetBoard();
-    this.GetCardData();//多次获取，稳定数据，可能删除
+ 
+    setTimeout(() => {
       this.changePage(e);
+    }, 500);
+     
 
     // this.setData({
     //   changedPageCounts: this.data.changedPageCounts + 1,
@@ -828,6 +831,7 @@ Page({
       }
     })
     // 换页部分
+    this.GetCardData();//多次获取，稳定数据，可能删除
     this.changePage(e);
     // this.setData({
     //   changedPageCounts: this.data.changedPageCounts + 1,
